e72c624146aa46b77e16145089d9f3a3a834b203
Stanley Fong <stanleyf@jfrog.com>
2016-07-14 14:07:20 -0700

    Changes for different xray environments

---
+++ xray/intro/xraygcp/dockerscan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Instruction - ./start.sh <docker tag>
+
+echo ">>> Downloading dependencies from artifactory for docker build"
+curl -uadmin:password -O https://xray-demo.jfrog.io/artifactory/jcenter/commons-io/commons-io/2.4/commons-io-2.4.jar
+curl -uadmin:password -O https://xray-demo.jfrog.io/artifactory/jcenter/uk/org/retep/xmpp/xmpp/commons/9.11/commons-9.11.jar
+
+echo ">>> Building docker image"
+docker login -u admin -p password xray-demo-docker-local.jfrog.io
+docker build -t xray-demo-docker-local.jfrog.io/xray/demo:$1 .
+docker push xray-demo-docker-local.jfrog.io/xray/demo:$1
+echo ">>> xray/demo deployed to artifactory"  
+
---
+++ xray/intro/xraygcp/mavenimpact.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Instruction - ./start.sh <docker tag>
+
+echo ">>> Create new xray maven events for impact analysis"
+curl -uadmin:password -H "Content-Type:application/json" -X POST http://10.200.11.11:8000/api/v1/events -T xray.json
+curl -uadmin:password -H "Content-Type:application/json" -X POST http://10.200.11.11:8000/api/v1/events -T xray2.json
+
