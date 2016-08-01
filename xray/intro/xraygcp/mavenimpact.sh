e89f42b2a2a569d80932c00a9094d8ad26c8d632
Stanley Fong <stanleyf@jfrog.com>
2016-08-01 15:54:18 -0700

    Add support for gcartifactory environment

--- xray/intro/xraygcp/mavenimpact.sh
+++ xray/intro/xraygcp/mavenimpact.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#Instruction - ./start.sh <docker tag>
 
 echo ">>> Create new xray maven events for impact analysis"
-curl -uadmin:password -H "Content-Type:application/json" -X POST http://10.200.11.11:8000/api/v1/events -T xray.json
-curl -uadmin:password -H "Content-Type:application/json" -X POST http://10.200.11.11:8000/api/v1/events -T xray2.json
+curl -uadmin:password -H "Content-Type:application/json" -X POST http://104.154.238.5:8000/api/v1/events -T xray.json
+curl -uadmin:password -H "Content-Type:application/json" -X POST http://104.154.238.5:8000/api/v1/events -T xray2.json
 
