a314dfa0644b46a947575d376784f004cf472db2
Stanley Fong <stanleyf@jfrog.com>
2016-08-03 17:16:29 -0700

    DNS related changes

--- xray/intro/xraygcp/mavenimpact.sh
+++ xray/intro/xraygcp/mavenimpact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo ">>> Create new xray maven events for impact analysis"
-curl -uadmin:password -H "Content-Type:application/json" -X POST http://104.154.238.5:8000/api/v1/events -T xray.json
-curl -uadmin:password -H "Content-Type:application/json" -X POST http://104.154.238.5:8000/api/v1/events -T xray2.json
+curl -uadmin:jfrog3945 -H "Content-Type:application/json" -X POST http://gcxray-us.jfrog.info:8000/api/v1/events -T xray.json
+curl -uadmin:jfrog3945 -H "Content-Type:application/json" -X POST http://gcxray-us.jfrog.info:8000/api/v1/events -T xray2.json
 
