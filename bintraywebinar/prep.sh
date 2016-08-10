c38a70aff6fb20275157043da7bdc7433c6e4a23
Mark Galpin <markg@jfrog.com>
2016-08-10 16:56:04 -0700

    Creating bintrayrocks

--- bintraywebinar/prep.sh
+++ bintraywebinar/prep.sh
@@ -1,4 +1,7 @@
 cp /usr/local/bin/jfrog .
+cd bintrayrocks
+tar -cvf bintrayrocks-1.0.tgz exec
+cd ..
 cp bintrayrocks/bintrayrocks.tgz .
 jfrog rt u bintrayrocks-1.0.tgz generic-local/jfrog/bintray/bintray-webinar/bintrayrocks/
 docker build -t gcartifactory-us.jfrog.info:5002/bintray-demo:1.0 .
