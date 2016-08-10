241d3de53214c6c9f34f2aff6c76377c4c49a614
Mark Galpin <markg@jfrog.com>
2016-08-10 16:52:21 -0700

    Adding bintray-webinar demo

---
+++ bintraywebinar/prep.sh
@@ -0,0 +1,6 @@
+cp /usr/local/bin/jfrog .
+cp bintrayrocks/bintrayrocks.tgz .
+jfrog rt u bintrayrocks-1.0.tgz generic-local/jfrog/bintray/bintray-webinar/bintrayrocks/
+docker build -t gcartifactory-us.jfrog.info:5002/bintray-demo:1.0 .
+docker push gcartifactory-us.jfrog.info:5002/bintray-demo:1.0
+jfrog bt acc-keys --org=jfrog-int create webinar-test
