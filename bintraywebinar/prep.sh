e42c25ed0d0da5a387d8f3665d662e308e12d5d5
Mark Galpin <markg@jfrog.com>
2016-08-10 18:11:34 -0700

    Split up prep into prep and commands

--- bintraywebinar/prep.sh
+++ bintraywebinar/prep.sh
@@ -4,8 +4,3 @@ tar -cvf bintrayrocks-1.0.tgz exec
 cd ..
 cp bintrayrocks/bintrayrocks.tgz .
 jfrog rt u bintrayrocks-1.0.tgz generic-local/jfrog/bintray/bintray-webinar/bintrayrocks/
-docker build -t gcartifactory-us.jfrog.info:5002/bintray-demo:1.0 .
-docker push gcartifactory-us.jfrog.info:5002/bintray-demo:1.0
-jfrog bt acc-keys --org=jfrog-int create webinar-key2
-jfrog bt ent --access=r --keys=webinar-key2 create jfrog-int/bintray-webinar-test2-gen
-jfrog bt ent --access=r --keys=webinar-key2 create jfrog-int/bintray-webinar-test2-reg
