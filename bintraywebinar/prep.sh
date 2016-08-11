75c8b70379b38ba8ba92b669da49e723221a6570
Mark Galpin <markg@jfrog.com>
2016-08-10 23:44:41 -0700

    Updating the product to be amazing

--- bintraywebinar/prep.sh
+++ bintraywebinar/prep.sh
@@ -1,6 +1,8 @@
 cp /usr/local/bin/jfrog .
 cd bintrayrocks
-tar -cvf bintrayrocks-1.0.tgz exec
+tar -cvf amaze-1.0.tgz exec
 cd ..
-cp bintrayrocks/bintrayrocks.tgz .
-jfrog rt u bintrayrocks-1.0.tgz generic-local/jfrog/bintray/bintray-webinar/bintrayrocks/
+cp bintrayrocks/amaze-1.0.tgz .
+jfrog rt u amaze-1.0.tgz generic-local/jfrog/bintray/bintray-webinar/amaze/
+docker build -t gcartifactory-us.jfrog.info:5002/amaze:1.0 .
+docker push gcartifactory-us.jfrog.info:5002/amaze:1.0
