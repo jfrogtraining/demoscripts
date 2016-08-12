18c0a200f0b9b0ddd0a4479569e5936eb3dedc89
Stanley Fong <stanleyf@jfrog.com>
2016-08-12 11:28:40 -0700

    script updated per environment

--- xray/intro/xraygcp/dockerscan.sh
+++ xray/intro/xraygcp/dockerscan.sh
@@ -2,11 +2,11 @@
 #Instruction - ./start.sh <docker tag>
 
 echo ">>> Downloading dependencies from artifactory for docker build"
-curl -H "X-JFrog-Art-Api:AKCp2UPLgsEE4gEfqrwyrG6Bs6N2bvX4autoYcQyXXEpSnwkuYQ9epZdy4aqtdoA7LxMqh2xP" -O https://gcartifactory-us.jfrog.info/artifactory/jcenter/commons-io/commons-io/2.4/commons-io-2.4.jar
-curl -H "X-JFrog-Art-Api:AKCp2UPLgsEE4gEfqrwyrG6Bs6N2bvX4autoYcQyXXEpSnwkuYQ9epZdy4aqtdoA7LxMqh2xP" -O https://gcartifactory-us.jfrog.info/artifactory/jcenter/uk/org/retep/xmpp/xmpp/commons/9.11/commons-9.11.jar
+curl -H "X-JFrog-Art-Api:<api key>" -O https://gcartifactory-us.jfrog.info/artifactory/jcenter/commons-io/commons-io/2.4/commons-io-2.4.jar
+curl -H "X-JFrog-Art-Api:<api key>" -O https://gcartifactory-us.jfrog.info/artifactory/jcenter/uk/org/retep/xmpp/xmpp/commons/9.11/commons-9.11.jar
 
 echo ">>> Building docker image"
-docker login -u stanleyf -p AKCp2UPLgsEE4gEfqrwyrG6Bs6N2bvX4autoYcQyXXEpSnwkuYQ9epZdy4aqtdoA7LxMqh2xP gcartifactory-us.jfrog.info:5002
+docker login -u <user> -p <password> gcartifactory-us.jfrog.info:5002
 docker build -t gcartifactory-us.jfrog.info:5002/xray/demo:$1 .
 docker push gcartifactory-us.jfrog.info:5002/xray/demo:$1
 echo ">>> xray/demo deployed to artifactory"  
--- xray/intro/xraygcp/mavenimpact.sh
+++ xray/intro/xraygcp/mavenimpact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo ">>> Create new xray maven events for impact analysis"
-curl -uadmin:jfrog3945 -H "Content-Type:application/json" -X POST http://gcxray-us.jfrog.info:8000/api/v1/events -T xray.json
-curl -uadmin:jfrog3945 -H "Content-Type:application/json" -X POST http://gcxray-us.jfrog.info:8000/api/v1/events -T xray2.json
+curl -u<user>:<password> -H "Content-Type:application/json" -X POST http://gcxray-us.jfrog.info:8000/api/v1/events -T xray.json
+curl -u<user>:<password> -H "Content-Type:application/json" -X POST http://gcxray-us.jfrog.info:8000/api/v1/events -T xray2.json
 
