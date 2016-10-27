52f1873d808982445ddb47806bc2dd61a06dc6fd
Mark Galpin <markg@jfrog.com>
2016-10-27 11:02:56 -0700

    Updating to add a pattern spec

--- cliwebinar/cliscript.sh
+++ cliwebinar/cliscript.sh
@@ -1,4 +1,7 @@
-jfrog rt dl gradle-dev-local/org/jfrog/example/gradle/api/1.1/ --build-name=gradle-example-ci-server --build-number=12
+export BUILD_NUM=1
+
+jfrog rt dl gradle-dev-local/org/jfrog/example/gradle/api/1.1/ --build-name=cli-example --build-number=$BUILD_NUM
+jfrog rt dl --spec=spec.json --build-name=cli-example --build-number=$BUILD_NUM
 
 jfrog rt bce gradle-example-ci-server 12
 
